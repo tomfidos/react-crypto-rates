f1f1731fed62f2c1ae6995ed18eeb366c7013863
Tomasz Fidos <fidostomasz@gmail.com>
2023-02-23 18:57:25 +0100

    Added Crypto component.

---
+++ src/Crypto.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './Crypto.css';
+
+class Crypto extends Component {
+
+    render() {
+        return(
+            <div>
+               <input type="text" placeholder='Filter'/> 
+            </div>
+        );
+    }
+}
+
+export default Crypto;
