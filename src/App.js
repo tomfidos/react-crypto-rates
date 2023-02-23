6142ca33aa5b52a1492835b3bd792dcf91005c6e
Tomasz Fidos <fidostomasz@gmail.com>
2023-02-23 18:47:02 +0100

    Added basic structure, header and styling.

--- src/App.js
+++ src/App.js
@@ -3,6 +3,10 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+      <header>
+        <img alt="BTC logo"></img>
+        <h1>crypto rate</h1>
+      </header>
     </div>
   );
 }
