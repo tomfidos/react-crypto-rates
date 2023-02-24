e3172f40086e8684c7097afff305802b26cd9661
Tomasz Fidos <fidostomasz@gmail.com>
2023-02-24 17:43:21 +0100

    Added MVP version of rate presentation.

--- src/CryptoList.js
+++ src/CryptoList.js
@@ -1,11 +1,13 @@
 import React from 'react';
+
 import './CryptoList.css';
 
 const CryptoList = (props) => {
+    const rateList = props.rateList.map(currency => <li key={currency.id}>Last rate: <span className="rate">{currency.rate} </span><span className="symbol">{currency.name} {currency.symbol}</span></li>);
 
     return(
         <ul>
-        
+            {rateList}
         </ul>
     );
 } 
