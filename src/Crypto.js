a03534f7e520e11311c662c6d6c21bc7de995df2
Tomasz Fidos <fidostomasz@gmail.com>
2023-02-24 18:31:12 +0100

    Replaced rate list with an object and added rate comparison.

--- src/Crypto.js
+++ src/Crypto.js
@@ -10,25 +10,31 @@ class Crypto extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            rateList: [],
+            rates: {},
         };
     }
 
     getRates = () => {
         axios.get(baseUrl, {mode: 'cors'}).then(respone => {
             const data = respone.data;
-            const reformattedData = [];
+            const currentRates = this.state.rates;
             for (let sym in data) {
-                reformattedData.push({
-                    id: Date.now().toString() + '_' + data[sym].symbol,
-                    name: sym,
-                    rate: data[sym].last,
-                    symbol: '',
-                });
+                if (!currentRates.hasOwnProperty(sym)) {
+                    currentRates[sym] = {};
+                    currentRates[sym]['direction'] = '';
+                    currentRates[sym]['rate'] = 0;
+                }
+                const currentRate = currentRates[sym]['rate'];
+                const lastRate = data[sym].last;
+                currentRates[sym]['id'] = Date.now().toString() + '_' + data[sym].symbol;
+                currentRates[sym]['name'] = sym;
+                currentRates[sym]['symbol'] = '';  // TODO
+                currentRates[sym]['direction'] = currentRate < lastRate * 0.999 ? 'down' : currentRate > lastRate * 1.001 ? 'up' : 'stable';  // TODO
+                currentRates[sym]['rate'] = lastRate;
             }
-            this.setState(state => {
+            this.setState(() => {
                 return({
-                    rateList: state.rateList.concat(reformattedData)
+                    rates: currentRates,
                 });
             });
         });
@@ -36,21 +42,22 @@ class Crypto extends Component {
 
     componentDidMount() {
         this.getRates();
+        console.log('component mounted');
     }
 
     componentDidUpdate() {
-
     }
 
     componentWillUnmount() {
-        
+        clearInterval(this.timerId);
+        console.log('compenent unmounted');
     }
 
     render() {
         return(
             <div>
                <input type="text" placeholder='Filter'/> 
-               <CryptoList rateList={this.state.rateList}/>
+               <CryptoList rates={this.state.rates}/>
             </div>
         );
     }
