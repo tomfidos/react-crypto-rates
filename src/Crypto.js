b90716b58f7bcf23445b02cf592f9faa7801173d
Tomasz Fidos <fidostomasz@gmail.com>
2023-02-24 17:43:21 +0100

    Added MVP version of rate presentation.

--- src/Crypto.js
+++ src/Crypto.js
@@ -1,14 +1,56 @@
 import React, { Component } from 'react';
+import axios from 'axios';
+
 import './Crypto.css';
 import CryptoList from './CryptoList';
 
+const baseUrl = 'https://blockchain.info/ticker';
+
 class Crypto extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            rateList: [],
+        };
+    }
+
+    getRates = () => {
+        axios.get(baseUrl, {mode: 'cors'}).then(respone => {
+            const data = respone.data;
+            const reformattedData = [];
+            for (let sym in data) {
+                reformattedData.push({
+                    id: Date.now().toString() + '_' + data[sym].symbol,
+                    name: sym,
+                    rate: data[sym].last,
+                    symbol: '',
+                });
+            }
+            this.setState(state => {
+                return({
+                    rateList: state.rateList.concat(reformattedData)
+                });
+            });
+        });
+    }
+
+    componentDidMount() {
+        this.getRates();
+    }
+
+    componentDidUpdate() {
+
+    }
+
+    componentWillUnmount() {
+        
+    }
 
     render() {
         return(
             <div>
                <input type="text" placeholder='Filter'/> 
-               <CryptoList />
+               <CryptoList rateList={this.state.rateList}/>
             </div>
         );
     }
