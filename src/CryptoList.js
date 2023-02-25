4fe2f50dae4e30f1a3035ef921a1fda2ed0a8a6b
Tomasz Fidos <fidostomasz@gmail.com>
2023-02-25 18:48:48 +0100

    Added colors for rate change direction.

--- src/CryptoList.js
+++ src/CryptoList.js
@@ -7,7 +7,7 @@ const CryptoList = (props) => {
     const rateCollection = [];
     for (const sym in rates) {
         const rate = rates[sym];
-        rateCollection.push(<li key={rate.id}>Last rate: <span className="rate">{rate.rate} {rate.direction} </span><span className="symbol">{rate.name} {rate.symbol}</span></li>);
+        rateCollection.push(<li key={rate.id}>Last rate: <span className={`rate ${rate.class}`}>{rate.rate} {rate.direction} </span><span className="symbol">{rate.name} {rate.symbol}</span></li>);
     }
 
     return(
